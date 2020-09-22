0861bd5f5efdaab0d4993657dd760d42f06e7b83
nianze <nianzelee@gmail.com>
2020-09-22 17:54:11 +0800

    Release a reference implementation of PA1
    Identify necessary APIs for PA1
    
    This commit prints the phases of a fanin in all cubes.
    It uses the APIs necessary for PA1.
    The circuit full_adder.blif is added as a simple test.
    
    In essence, students need to know:
    1) How SOP is stored as a char*
    2) How to decide whether the SOP represents onset or offset
    3) How to read the phase of a variable in a cube
    
    Modify the full adder blif file
    
    Rename the PI/PO (c-in, c-out); delete redundant cube "111 1" from MAJ
    
    Fix null pointer exception when the network does not have SOP
    
    Delete full_adder.blif in the root directory of ABC
    
    Define a testing alias `lsv' in abc.rc
    
    Finish a reference implemenation for PA1

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -2,10 +2,15 @@
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
 
+extern void Lsv_NtkPrintSopUnate(Abc_Ntk_t*);
+
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandPrintSopUnate(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_sopunate", Lsv_CommandPrintSopUnate,
+                 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -21,14 +26,25 @@ void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
   int i;
   Abc_NtkForEachNode(pNtk, pObj, i) {
     printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
+    if (Abc_NtkHasSop(pNtk)) {
+      char* pSop = (char*)pObj->pData;
+      printf("The SOP of this node:\n%s", pSop);
+      printf("The phase of the SOP: %d\n", Abc_SopGetPhase(pSop));
+    }
     Abc_Obj_t* pFanin;
     int j;
     Abc_ObjForEachFanin(pObj, pFanin, j) {
       printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
              Abc_ObjName(pFanin));
-    }
-    if (Abc_NtkHasSop(pNtk)) {
-      printf("The SOP of this node:\n%s", (char*)pObj->pData);
+      if (Abc_NtkHasSop(pNtk)) {
+        printf("  Phase of this fanin:");
+        char* pSop = (char*)pObj->pData;
+        char* pCube;
+        Abc_SopForEachCube(pSop, Abc_SopGetVarNum(pSop), pCube) {
+          printf(" %c", pCube[j]);
+        }
+        printf("\n");
+      }
     }
   }
 }
@@ -55,6 +71,38 @@ int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
 usage:
   Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
   Abc_Print(-2, "\t        prints the nodes in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
+
+int Lsv_CommandPrintSopUnate(Abc_Frame_t* pAbc, int argc, char** argv) {
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+    switch (c) {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk) {
+    Abc_Print(-1, "Empty network.\n");
+    return 1;
+  }
+  if (!Abc_NtkHasSop(pNtk)) {
+    Abc_Print(-1, "The network does not have SOP.\n");
+    return 1;
+  }
+  Lsv_NtkPrintSopUnate(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_print_sopunate [-h]\n");
+  Abc_Print(-2,
+            "\t        prints the unate information of the SOP nodes in the "
+            "network\n");
   Abc_Print(-2, "\t-h    : print the command usage\n");
   return 1;
 }
--- src/ext-lsv/module.make
+++ src/ext-lsv/module.make
@@ -1,2 +1,3 @@
 SRC += \
-    src/ext-lsv/lsvCmd.cpp
+    src/ext-lsv/lsvCmd.cpp \
+    src/ext-lsv/lsvUnate.cpp
