af45f6d524375a3cc287b74277c0f03500b2cdc6
nianze <nianzelee@gmail.com>
2020-10-19 13:59:43 +0800

    Change lockdown enable/disable scripts
    The lockdown of master is always enabled.
    Only toggles students' branches.

--- lsv/admin/disable-lockdown.sh
+++ lsv/admin/disable-lockdown.sh
@@ -1,10 +1,15 @@
-if [ -f ".github/workflows/lockdown.yml" ]; then
-    git sw master
-    mv .github/workflows/lockdown.yml .github/workflows/lockdown.yml.disabled
-    git a .github/workflows
-    git cm "Disable lockdown"
-    git push
-    lsv/admin/update-branches.sh
-else
-    echo "[WARNING] Lockdown is NOT enabled!"
-fi
+students=( $(cut -d, -f1 < lsv/admin/participants-id.csv | tail -n +3) )
+for student in "${students[@]}"; do
+    git sw "${student}"
+    echo "Disable lockdown for branch ${student} ..."
+    if [ -f ".github/workflows/lockdown.yml" ]; then
+        mv .github/workflows/lockdown.yml .github/workflows/lockdown.yml.disabled
+        git a .github/workflows
+        git cm "Disable lockdown"
+        git push
+    else
+        echo "[ERROR] Lockdown is NOT enabled!"
+        exit 1
+    fi
+done
+git sw master
--- lsv/admin/enable-lockdown.sh
+++ lsv/admin/enable-lockdown.sh
@@ -1,10 +1,15 @@
-if [ -f ".github/workflows/lockdown.yml.disabled" ]; then
-    git sw master
-    mv .github/workflows/lockdown.yml.disabled .github/workflows/lockdown.yml
-    git a .github/workflows
-    git cm "Enable lockdown"
-    git push
-    lsv/admin/update-branches.sh
-else
-    echo "[WARNING] Lockdown is NOT disabled!"
-fi
+students=( $(cut -d, -f1 < lsv/admin/participants-id.csv | tail -n +3) )
+for student in "${students[@]}"; do
+    git sw "${student}"
+    echo "Enable lockdown for branch ${student} ..."
+    if [ -f ".github/workflows/lockdown.yml.disabled" ]; then
+        mv .github/workflows/lockdown.yml.disabled .github/workflows/lockdown.yml
+        git a .github/workflows
+        git cm "Enable lockdown"
+        git push
+    else
+        echo "[ERROR] Lockdown is NOT disabled!"
+        exit 1
+    fi
+done
+git sw master
