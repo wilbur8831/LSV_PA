5c6832070b69c065ac4eed15d6727834db8e02b9
nianze <nianzelee@gmail.com>
2020-10-19 13:20:43 +0800

    Simplify the script to update branches

--- lsv/admin/update-branches.sh
+++ lsv/admin/update-branches.sh
@@ -1,9 +1,8 @@
 students=( $(cut -d, -f1 < lsv/admin/participants-id.csv | tail -n +3) )
-git sw master
 for student in "${students[@]}"; do
     echo "Updating branch ${student} ..."
     git sw "${student}"
     git merge master
     git push
-    git sw master
 done
+git sw master
