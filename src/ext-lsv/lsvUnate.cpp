679f131e32a00b57beefb9a377a7ccd129e03122
nianze <nianzelee@gmail.com>
2020-11-03 22:20:52 +0800

    Improve the implementation with coning
    For each PO, create its TFI cone and only put this cone into SAT solver.
    
    TODO:
    - PIs might be reordered. Need to fix this.
    - Refactor the code.

--- src/ext-lsv/lsvUnate.cpp
+++ src/ext-lsv/lsvUnate.cpp
@@ -19,18 +19,16 @@ static int Vec_PtrSortCompareObjId(void** pp1, void** pp2) {
 
 static void writeCnfIntoSolver(Cnf_Dat_t*, sat_solver*);
 static bool isUnate(sat_solver*, Cnf_Dat_t*, Cnf_Dat_t*, Vec_Int_t*, Aig_Man_t*,
-                    int, int, bool);
+                    int, bool);
 static bool isPositiveUnate(sat_solver* pSat, Cnf_Dat_t* pCnfPos,
                             Cnf_Dat_t* pCnfNeg, Vec_Int_t* pVecEnVars,
-                            Aig_Man_t* pMan, int pIndexCo, int pIndexCi) {
-  return isUnate(pSat, pCnfPos, pCnfNeg, pVecEnVars, pMan, pIndexCo, pIndexCi,
-                 true);
+                            Aig_Man_t* pMan, int pIndexCi) {
+  return isUnate(pSat, pCnfPos, pCnfNeg, pVecEnVars, pMan, pIndexCi, true);
 }
 static bool isNegativeUnate(sat_solver* pSat, Cnf_Dat_t* pCnfPos,
                             Cnf_Dat_t* pCnfNeg, Vec_Int_t* pVecEnVars,
-                            Aig_Man_t* pMan, int pIndexCo, int pIndexCi) {
-  return isUnate(pSat, pCnfPos, pCnfNeg, pVecEnVars, pMan, pIndexCo, pIndexCi,
-                 false);
+                            Aig_Man_t* pMan, int pIndexCi) {
+  return isUnate(pSat, pCnfPos, pCnfNeg, pVecEnVars, pMan, pIndexCi, false);
 }
 
 void Lsv_NtkPrintSopUnate(Abc_Ntk_t* pNtk) {
@@ -114,36 +112,44 @@ void printObjNameInVec(Vec_Ptr_t* pVec) {
 }
 
 void Lsv_NtkPrintPoUnate(Abc_Ntk_t* pNtk) {
-  Aig_Man_t* pMan = Abc_NtkToDar(pNtk, 0, 0);
-  Cnf_Dat_t* pCnfPositiveCofactor = Cnf_Derive(pMan, Aig_ManCoNum(pMan));
-  Cnf_Dat_t* pCnfNegativeCofactor = Cnf_DataDup(pCnfPositiveCofactor);
-  Cnf_DataLift(pCnfNegativeCofactor, pCnfPositiveCofactor->nVars);
-  sat_solver* pSatSolver = sat_solver_new();
-  sat_solver_setnvars(
-      pSatSolver, pCnfPositiveCofactor->nVars + pCnfNegativeCofactor->nVars);
-  writeCnfIntoSolver(pCnfPositiveCofactor, pSatSolver);
-  writeCnfIntoSolver(pCnfNegativeCofactor, pSatSolver);
-  Vec_Int_t* vEnableVars = Vec_IntAlloc(Aig_ManCiNum(pMan));
-  Aig_Obj_t *pCi, *pCo;
-  int i, j;
-  Aig_ManForEachCi(pMan, pCi, i) {
-    Vec_IntPush(vEnableVars, sat_solver_addvar(pSatSolver));
-    sat_solver_add_buffer_enable(pSatSolver,
-                                 pCnfPositiveCofactor->pVarNums[Aig_ObjId(pCi)],
-                                 pCnfNegativeCofactor->pVarNums[Aig_ObjId(pCi)],
-                                 Vec_IntEntryLast(vEnableVars), 0);
-  }
-  Aig_ManForEachCo(pMan, pCo, i) {
+  Abc_Obj_t* pPo;
+  int i;
+  Abc_NtkForEachPo(pNtk, pPo, i) {
+    // printf("po %s:\n", Abc_ObjName(pPo));
+    Abc_Ntk_t* pNtkCone =
+        Abc_NtkCreateCone(pNtk, Abc_ObjFanin0(pPo), Abc_ObjName(pPo), 0);
+    if (Abc_ObjFaninC0(pPo)) {
+      Abc_ObjSetFaninC(Abc_NtkPo(pNtkCone, 0), 0);
+    }
+    Aig_Man_t* pMan = Abc_NtkToDar(pNtkCone, 0, 0);
+    Cnf_Dat_t* pCnfPositiveCofactor = Cnf_Derive(pMan, Aig_ManCoNum(pMan));
+    Cnf_Dat_t* pCnfNegativeCofactor = Cnf_DataDup(pCnfPositiveCofactor);
+    Cnf_DataLift(pCnfNegativeCofactor, pCnfPositiveCofactor->nVars);
+    sat_solver* pSatSolver = sat_solver_new();
+    sat_solver_setnvars(
+        pSatSolver, pCnfPositiveCofactor->nVars + pCnfNegativeCofactor->nVars);
+    writeCnfIntoSolver(pCnfPositiveCofactor, pSatSolver);
+    writeCnfIntoSolver(pCnfNegativeCofactor, pSatSolver);
+    Vec_Int_t* vEnableVars = Vec_IntAlloc(Aig_ManCiNum(pMan));
+    Aig_Obj_t* pCi;
+    int j;
+    Aig_ManForEachCi(pMan, pCi, j) {
+      Vec_IntPush(vEnableVars, sat_solver_addvar(pSatSolver));
+      sat_solver_add_buffer_enable(
+          pSatSolver, pCnfPositiveCofactor->pVarNums[Aig_ObjId(pCi)],
+          pCnfNegativeCofactor->pVarNums[Aig_ObjId(pCi)],
+          Vec_IntEntryLast(vEnableVars), 0);
+    }
     Aig_ManForEachCi(pMan, pCi, j) {
       bool isPosUnate = false;
       bool isNegUnate = false;
       if (isPositiveUnate(pSatSolver, pCnfPositiveCofactor,
-                          pCnfNegativeCofactor, vEnableVars, pMan, i, j)) {
+                          pCnfNegativeCofactor, vEnableVars, pMan, j)) {
         isPosUnate = true;
         // printf("[INFO] PO %d is positive unate in PI %d\n", i, j);
       }
       if (isNegativeUnate(pSatSolver, pCnfPositiveCofactor,
-                          pCnfNegativeCofactor, vEnableVars, pMan, i, j)) {
+                          pCnfNegativeCofactor, vEnableVars, pMan, j)) {
         isNegUnate = true;
         // printf("[INFO] PO %d is negative unate in PI %d\n", i, j);
       }
@@ -156,12 +162,13 @@ void Lsv_NtkPrintPoUnate(Abc_Ntk_t* pNtk) {
       }*/
     }
     printf("\n");
+    Aig_ManStop(pMan);
+    Cnf_DataFree(pCnfPositiveCofactor);
+    Cnf_DataFree(pCnfNegativeCofactor);
+    sat_solver_delete(pSatSolver);
+    Vec_IntFree(vEnableVars);
+    Abc_NtkDelete(pNtkCone);
   }
-  Aig_ManStop(pMan);
-  Cnf_DataFree(pCnfPositiveCofactor);
-  Cnf_DataFree(pCnfNegativeCofactor);
-  sat_solver_delete(pSatSolver);
-  Vec_IntFree(vEnableVars);
 }
 
 void writeCnfIntoSolver(Cnf_Dat_t* pCnf, sat_solver* pSat) {
@@ -171,7 +178,7 @@ void writeCnfIntoSolver(Cnf_Dat_t* pCnf, sat_solver* pSat) {
 }
 
 bool isUnate(sat_solver* pSat, Cnf_Dat_t* pCnfPos, Cnf_Dat_t* pCnfNeg,
-             Vec_Int_t* pVecEnVars, Aig_Man_t* pMan, int pIndexCo, int pIndexCi,
+             Vec_Int_t* pVecEnVars, Aig_Man_t* pMan, int pIndexCi,
              bool pPhase) {
   lit assumptions[Aig_ManCiNum(pMan) + 2 + 2];
   int Entry, i;
@@ -183,10 +190,10 @@ bool isUnate(sat_solver* pSat, Cnf_Dat_t* pCnfPos, Cnf_Dat_t* pCnfNeg,
       toLitCond(pCnfPos->pVarNums[Aig_ObjId(Aig_ManCi(pMan, pIndexCi))], 0);
   assumptions[Aig_ManCiNum(pMan) + 1] =
       toLitCond(pCnfNeg->pVarNums[Aig_ObjId(Aig_ManCi(pMan, pIndexCi))], 1);
-  assumptions[Aig_ManCiNum(pMan) + 2] = toLitCond(
-      pCnfPos->pVarNums[Aig_ObjId(Aig_ManCo(pMan, pIndexCo))], pPhase);
-  assumptions[Aig_ManCiNum(pMan) + 3] = toLitCond(
-      pCnfNeg->pVarNums[Aig_ObjId(Aig_ManCo(pMan, pIndexCo))], !pPhase);
+  assumptions[Aig_ManCiNum(pMan) + 2] =
+      toLitCond(pCnfPos->pVarNums[Aig_ObjId(Aig_ManCo(pMan, 0))], pPhase);
+  assumptions[Aig_ManCiNum(pMan) + 3] =
+      toLitCond(pCnfNeg->pVarNums[Aig_ObjId(Aig_ManCo(pMan, 0))], !pPhase);
   if (sat_solver_solve(pSat, assumptions, assumptions + Aig_ManCiNum(pMan) + 4,
                        0, 0, 0, 0) == l_False) {
     return true;
