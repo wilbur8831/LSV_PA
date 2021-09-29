05310b618a91a28ede71f02d86e4ff243c55e864
Luo-Yun-Rong <b06901073@ntu.edu.tw>
2021-09-29 10:58:13 +0800

    Rename lsv to archive

--- lsv/admin/delete-branches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-students=( $(cut -d, -f1 < lsv/admin/participants-id.csv | tail -n +3) )
-git switch master
-for student in "${students[@]}"; do
-    echo "Deleting branch ${student} ..."
-    git branch -d "${student}"
-    git push origin --delete "${student}"
-done
